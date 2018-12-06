65351028cfa6baf60b2708656240e8170e1cde5e
Tamas Benkoe <tamas.benkoe@1und1.de>
2018-12-06 23:31:28 +0100

    Finish day 5

--- src/advent_of_code_2018/day5/code.clj
+++ src/advent_of_code_2018/day5/code.clj
@@ -1,19 +1,27 @@
-(ns advent-of-code-2018.day5.code)
+(ns advent-of-code-2018.day5.code
+  (:require [clojure.string :as str]))
 
 (defonce input-file "src/advent_of_code_2018/day5/input.txt")
 
-(defonce input (slurp input-file))
+(defonce input (str/trim-newline (slurp input-file)))
 
 (defn react [polymer]
   (let [b (StringBuilder. polymer)]
     (loop [i 0]
       (if (< i (dec (count b)))
-        (if (= 32 (Math/abs (- (int (.charAt b i)) (int (.charAt b (inc i))))))
+        (if (= 32 (Math/abs (- (.codePointAt b i) (.codePointAt b (inc i)))))
           (do (.delete b i (+ i 2))
               (recur (max (dec i) 0)))
           (recur (inc i)))
         (str b)))))
 
-(react "dabAcCaCBAcCcaDA")
+(defonce problem1 (count (react input)))
 
-(count (react input))
+(defonce problem2
+  (->> (for [i (range (int \a) (inc (int \z)))
+             :let [c (char i)
+                   s (str/replace input (str c) "")
+                   s (str/replace s (str (char (- i 32))) "")]]
+         [c (count (react s))])
+       (apply min-key second)
+       last))
