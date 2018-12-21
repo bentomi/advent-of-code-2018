a78c7d31372ab20275c7df792777b1449caa97ee
Tamas Benkoe <tamas.benkoe@1und1.de>
2018-12-21 23:44:34 +0100

    Continue day 15 - support move, attack, turn

--- src/advent_of_code_2018/day15/code.clj
+++ src/advent_of_code_2018/day15/code.clj
@@ -22,14 +22,17 @@
               :when race]
           [[y x] {:race race, :hp 200}])))
 
-(defn find-targets [{:keys [race]} {:keys [warriors]}]
+(defn find-targets [{:keys [race]} warriors]
   (map first (filter #(-> % second :race #{(other-race race)}) warriors)))
 
 (defn adjacent [[y x]]
   [[(dec y) x] [y (dec x)] [y (inc x)] [(inc y) x]])
 
+(defn match-adjacent [pred field pos]
+  (filter #(pred (get-in field %)) (adjacent pos)))
+
 (defn free-adjacent [field pos]
-  (filter #(= free (get-in field %)) (adjacent pos)))
+  (match-adjacent #{free} field pos))
 
 (defn bfs [field start-pos targets]
   (loop [q (conj clojure.lang.PersistentQueue/EMPTY [start-pos []])
@@ -49,7 +52,55 @@
         solutions))))
 
 (defn warrior-move [{:keys [field warriors]} pos]
-  (let [targets (find-targets (get-in field pos) warriors)
-        ;; test if a target is already in range, else move
-        in-range (set (mapcat #(free-adjacent field %) targets))]
-    (first (sort-by (juxt peek first) (bfs field start-pos in-range)))))
+  (let [targets (find-targets (get warriors pos) warriors)]
+    (if (seq targets)
+      (let [field-without (assoc-in field pos \.)
+            in-range (set (mapcat #(free-adjacent field-without %) targets))]
+        (if (seq (filter #{pos} in-range))
+          :in-range
+          (if (seq in-range)
+            (or (->> (bfs field pos in-range)
+                     (sort-by (juxt peek identity))
+                     first
+                     second)
+                :no-path)
+            :no-reachable-target)))
+      :no-target)))
+
+(def attack-power 3)
+
+(defn add-warrior [state pos warrior]
+  (-> state
+      (assoc-in (cons :field pos) (:race warrior))
+      (update :warrior assoc pos warrior)))
+
+(defn remove-warrior [state pos]
+  (-> state
+      (assoc-in (cons :field pos) free)
+      (update :warrior dissoc pos)))
+
+(defn pick-enemy [{:keys [field warriors]} pos]
+  (let [enemy-race (other-race (get-in warriors [pos :race]))]
+    (->> (match-adjacent #{enemy-race} field pos)
+         (sort-by (juxt #(get-in warriors [% :hp]) identity))
+         first)))
+
+(defn attack [{:keys [field warriors] :as state} pos]
+  (if-let [enemy-pos (pick-enemy state pos)]
+    (if (<= (get-in warriors [enemy-pos :hp]) attack-power)
+      (remove-warrior state enemy-pos)
+      (update-in state [:warriors enemy-pos :hp] - attack-power))
+    state))
+
+(defn warrior-step [state from to]
+  (-> state
+      (remove-warrior from)
+      (add-warrior to (get-in state [:warriors from]))))
+
+(defn turn [{:keys [field warriors] :as state} pos]
+  (let [move (warrior-move state pos)]
+    (cond
+      (vector? move) (attack (warrior-step state pos move) move)
+      (= :in-range move) (attack state pos)
+      (= :no-target move) :battle-end
+      :else state)))
