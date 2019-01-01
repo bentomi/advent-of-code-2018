184871dd774f6561875d4a31b53f9f18311bcfe7
Tamas Benkoe <tamas.benkoe@1und1.de>
2019-01-02 00:11:45 +0100

    Produce stack overflow on problem 1 of day 17

---
+++ src/advent_of_code_2018/day17/code.clj
@@ -0,0 +1,85 @@
+(ns advent-of-code-2018.day17.code
+  (:require [clojure.java.io :as io]))
+
+(defonce line-pattern #"([xy])=(\d+), ([xy])=(\d+)\.\.(\d+)")
+
+(defn parse-line [line]
+  (let [[_ d0 pos d1 r0 r1] (re-matches line-pattern line)]
+    (array-map (keyword d0) (Integer/parseInt pos)
+               (keyword d1) [(Integer/parseInt r0) (Integer/parseInt r1)])))
+
+(defonce input
+  (with-open [rdr (io/reader "src/advent_of_code_2018/day17/input.txt")]
+    (->> rdr line-seq (mapv parse-line))))
+
+(def clay \#)
+(def sand \.)
+(def water \~)
+(def wet \|)
+
+(defn shift-left [shift {:keys [x y] :as line}]
+  (assoc line :x (if (vector? x) (mapv #(- % shift) x) (- x shift))))
+
+(let [xs (map #(let [x (:x %)] (if (vector? x) (second x) x)) input)
+      ys (map #(let [y (:y %)] (if (vector? y) (second y) y)) input)
+      min-x (dec (apply min xs))]
+  (def normalized-input (mapv (partial shift-left min-x) input))
+  (def spring [0 (- 500 min-x)])
+  (def max-x (- (inc (apply max xs)) min-x))
+  (def max-y (apply max ys)))
+
+(defn set-clay [scan {:keys [x y]}]
+  (if (vector? x)
+    (reduce #(assoc-in %1 [y %2] clay) scan (range (first x) (inc (second x))))
+    (reduce #(assoc-in %1 [%2 x] clay) scan (range (first y) (inc (second y))))))
+
+(def scan
+  (let [s (vec (repeat (inc max-y) (vec (repeat (inc max-x) sand))))
+        s (reduce set-clay s normalized-input)]
+    (assoc-in s spring \+)))
+
+(defn draw-scan [scan]
+  (doseq [r scan]
+    (doseq [c r]
+      (print c))
+    (println)))
+
+(defn up [[y x]] [(dec y) x])
+(defn down [[y x]] [(inc y) x])
+(defn left [[y x]] [y (dec x)])
+(defn right [[y x]] [y (inc x)])
+
+(defn below [[y] depth] (< depth y))
+
+(defn blocked [scan pos]
+  (#{clay water} (get-in scan pos)))
+
+(declare spread)
+
+(defn trickle [scan pos min-depth]
+  (let [d (down pos)]
+    (cond
+      (>= (first d) (count scan)) [:drawn scan]
+      (= sand (get-in scan d)) (recur (assoc-in scan d wet) d min-depth)
+      :else (let [[loutcome scan] (spread left scan pos wet)
+                  [routcome scan] (spread right scan pos wet)]
+              (if (some #{:drawn} [loutcome routcome])
+                [:drawn scan]
+                (let [[_ scan] (spread left scan pos water)
+                      [_ scan] (spread right scan pos water)]
+                  (if (below pos min-depth)
+                    (recur scan (up pos) min-depth)
+                    [:filled scan])))))))
+
+(defn spread [dir scan pos water]
+  (let [n (dir pos)]
+    (if (blocked scan n)
+      [:filled scan]
+      (let [scan (assoc-in scan n water)
+            nd (down n)]
+        (if (blocked scan nd)
+          (recur dir scan n water)
+          (let [[outcome scan :as tresult] (trickle scan n (inc (first n)))]
+            (if (= outcome :drawn)
+              tresult
+              (recur dir scan n water))))))))
